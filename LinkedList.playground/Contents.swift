2ac9e418d51e24cedadd7fa651fa2c9054749e8a
Emily Corso <ekcorso@gmail.com>
2024-06-17 10:38:34 -0700

    Start LinkedList implementation

---
+++ LinkedList.playground/Contents.swift
@@ -0,0 +1,14 @@
+// Basic implementation for a singly linked list
+
+import UIKit
+
+class Node {
+    var data: Int? = nil
+    var next: Node? = nil
+}
+
+class LinkedList {
+    var head: Node? = nil
+}
+
+
